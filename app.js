e788303bd91ce2ff038fd7278a693fd14940e87e
Johnson, Patty (SLC-MEW) <Patty.Johnson@mrmmccann.com>
2014-04-02 15:55:01 -0600

    added jade template language and goatlist views

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ var express = require('express'),
 	path = "public/",
 	port = 1025;
 
+var goatList = ['Betty', 'Mabel', 'Rex'];
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.get('/goats/', function(req, res){
+	console.log(req.url);
+	res.render('layout');
+});
+app.get('/goats/list/', function(req, res){
+	console.log(req.url);
+	res.render('goat_list', {
+		title: 'Goat List',
+		list: goatList
+	});
+});
+
 app.use(express.static(path));
 app.use(express.directory(path));
 
